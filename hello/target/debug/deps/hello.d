36b157d6baa139073bcc36c36669af67c5511ab0
Io <wmt.tkn.121@gmail.com>
2022-04-21 23:03:30 +0900

    hello world

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\io\Documents\workspace\RustExercise\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\Users\io\Documents\workspace\RustExercise\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
